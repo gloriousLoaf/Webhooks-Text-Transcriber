ba0d380cdf178e4eea90e5e2f877ad0ff4ca1740
gloriousLoaf <davidmetcalfwork@gmail.com>
2021-03-12 16:52:52 -0800

    Setup Discord & GitHub bots, deployed Netlify function.

--- code/netlify-discorder/functions/discorder/discorder.js
+++ code/netlify-discorder/functions/discorder/discorder.js
@@ -1,14 +1,19 @@
-const axios = require("axios").default;
+const axios = require('axios').default;
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
     const body = JSON.parse(event.body);
-    const username = body.sender.login;
     const avatarUrl = body.sender.avatar_url;
+    const userName = body.sender.login;
     const repoName = body.repository.name;
+    const repoUrl = `https://github.com/${body.repository.full_name}`;
+    const userGH = body.sender.html_url;
+    const content = `:star: Your repo ${repoName} was starred by ${userName}.
+    ${repoUrl}
+    ${userGH}`;
     const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-      content: `:taco: :taco: :taco: ${username} just starred ${repoName}! :rocket: :muscle: :tada: :taco:`,
+      content: content,
       embeds: [
         {
           image: {
@@ -17,7 +22,7 @@ exports.handler = async (event, context) => {
         },
       ],
     });
-    console.log("Submitted!");
+    console.log('Submitted!');
     return {
       statusCode: 204,
     };
